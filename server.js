740c76e764a81ac139b80573dccc0f3672f5108c
deepakrawat0007 <deepakrawat3738@gmail.com>
2022-12-13 21:25:17 +0530

    InstaClone Backend Part added to git

---
+++ server.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const port = 3000
+const app = require('./app')
+const dotenv = require('dotenv');
+dotenv.config();
+
+
+const API = process.env.DATABASE_URL || "mongodb://localhost/instaclone"
+async function main(){
+await mongoose.connect(API)
+  .then(() => console.log('Connected!'))
+
+
+
+app.listen(port , ()=>console.log("Server is Running..."))
+}
+main()
