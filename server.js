346bd1c8456a4f77a085d59e292cfcc1dcdcdaad
deepakrawat0007 <deepakrawat3738@gmail.com>
2022-12-14 09:26:10 +0530

    minor bugs fixes

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const port = 3000
+const port = 3001
 const app = require('./app')
 const dotenv = require('dotenv');
 dotenv.config();
@@ -9,9 +9,7 @@ const API = process.env.DATABASE_URL || "mongodb://localhost/instaclone"
 async function main(){
 await mongoose.connect(API)
   .then(() => console.log('Connected!'))
-
-
-
+  
 app.listen(port , ()=>console.log("Server is Running..."))
 }
 main()
